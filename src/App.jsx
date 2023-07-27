d0e59ce174fff81ee112716d7a37a83c5b8c1434
Shubh <shubhjhawar78@gmail.com>
2023-07-27 14:20:07 -0400

    browserRouter setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import react from 'react'
+import react from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
 
 function App() {
   return (
-    <div>
-      Shubh Jhawar Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className='bg-hero-patttern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
         
   )
 }
