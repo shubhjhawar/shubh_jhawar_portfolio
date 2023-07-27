365a09b44c30a2b5833f6f850efaef3c74de3c57
Shubh <shubhjhawar78@gmail.com>
2023-07-27 17:35:37 -0400

    navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import react from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
